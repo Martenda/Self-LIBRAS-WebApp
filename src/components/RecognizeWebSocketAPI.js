7c0664eb0d64a2d02e90b61b99e51fca38c89795
back <joaoback47@gmail.com>
2024-11-06 15:57:13 -0300

    deploy

--- src/components/RecognizeWebSocketAPI.js
+++ src/components/RecognizeWebSocketAPI.js
@@ -49,8 +49,8 @@ const RecognizeWebSocketAPI = () => {
 
     // const socketUrl = 'https://self-libras-2.fly.dev/';
     // const socketUrl = 'https://self-libras.azurewebsites.net/';
-    // const socketUrl = 'ws://4.155.126.87/live_camera';
-    const socketUrl = 'wss://137.135.6.252/live_camera';
+    const socketUrl = 'ws://20.112.58.223/live_camera';
+    //const socketUrl = 'wss://137.135.6.252/live_camera';
   
     socketRef.current = new WebSocket(socketUrl);
 
