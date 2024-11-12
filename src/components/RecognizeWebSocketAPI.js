2557eb3ec13e98cda5ee3696ff3482dd78b89dab
Lucas Martendal <lucasmartendal777@gmail.com>
2024-11-12 19:41:05 -0300

    Update RecognizeWebSocketAPI.js

--- src/components/RecognizeWebSocketAPI.js
+++ src/components/RecognizeWebSocketAPI.js
@@ -47,7 +47,7 @@ const RecognizeWebSocketAPI = () => {
     
     // const socketUrl = `${protocol}://${hostname}:${port}${path}`;
 
-    const socketUrl = 'https://self-libras-4.fly.dev/';
+    const socketUrl = 'https://self-libras-4.fly.dev/live_camera';
     // const socketUrl = 'https://self-libras.azurewebsites.net/';
     // const socketUrl = 'ws://20.112.58.223/live_camera';
     //const socketUrl = 'wss://137.135.6.252/live_camera';
